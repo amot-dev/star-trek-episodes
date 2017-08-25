716f3c0ec6440d1dd595d6ff179d906c796c75bf
Alexander Mot <asd777101@gmail.com>
2017-08-24 23:08:58 -0700

    Upload existing files

---
+++ js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    var season_nav_offset = $('#season-nav').offset().top;
+    $(window).scroll(function(){
+        if ($(window).scrollTop() >= season_nav_offset) {
+            $('#season-nav').addClass('sticky');
+        }
+        else {
+            $('#season-nav').removeClass('sticky');
+        }
+    });
+
+    $(document).on('click', 'a', function(event){
+    event.preventDefault();
+
+    if ($('#season-nav').hasClass('sticky')) {
+        var amount_to_scroll = $( $.attr(this, 'href') ).offset().top - $('#season-nav').outerHeight(true)
+    }
+    else {
+        var amount_to_scroll = $( $.attr(this, 'href') ).offset().top - $('#season-nav').outerHeight(true) - $('#season-nav').outerHeight(true)
+    }
+
+    $('html, body').animate({
+        scrollTop: amount_to_scroll
+    }, 500);
+
+});
+});
